68a115e4b2070f84015f94f29aa4c3b569e9b039
prakharv07 <97461633+prakharv07@users.noreply.github.com>
2024-07-18 23:47:51 +0530

    Update Chat.jsx

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -18,7 +18,7 @@ const Chat = () => {
 
   //request on loading
   useEffect(() => {
-    const url = "https://sih-r2-backend.onrender.com/dashboard";
+    const url = "https://langmodlabs-server.onrender.com/dashboard";
 
     const fetchdataChatup = async () => {
       try {
